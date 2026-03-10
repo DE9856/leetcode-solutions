488c422b8b4ff7659683037e8324bde98f1b776c
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-10 20:32:10 +0530

    Create 1480_running_sum_of_1d_array.py

---
+++ Python/1480_running_sum_of_1d_array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def runningSum(self, nums):
+        sum = 0
+        for i in range(len(nums)):
+            sum+=nums[i]
+            nums[i] = sum
+        return nums
+
+        
