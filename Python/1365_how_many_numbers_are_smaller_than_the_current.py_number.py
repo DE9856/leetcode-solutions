a236727e9e8838717bc7c212026f710bc9c48fa7
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-12 20:53:30 +0530

    Update 1365_how_many_numbers_are_smaller_than_the_current.py_number.py

--- Python/1365_how_many_numbers_are_smaller_than_the_current.py_number.py
+++ Python/1365_how_many_numbers_are_smaller_than_the_current.py_number.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def smallerNumbersThanCurrent(self, nums):
-        nums2 = nums
+        nums2 = nums[:]
         nums2.sort()
         for i in range(len(nums)):
             nums[i] = nums2.index(nums[i])
