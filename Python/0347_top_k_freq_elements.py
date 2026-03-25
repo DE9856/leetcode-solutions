c68a9b47168430ce6c761f19d00980d3ded20b99
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-25 19:12:04 +0530

    Implement topKFrequent using hashmaps
    Added an alternative implementation using hashmaps to find the top K frequent elements.

--- Python/0347_top_k_freq_elements.py
+++ Python/0347_top_k_freq_elements.py
@@ -7,3 +7,22 @@ class Solution(object):
         l1 = sorted(d1,key = d1.get,reverse = True)
 
         return l1[:k]
+
+
+# or using hashmaps
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        hashmap = {}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num]+=1
+            else:
+                hashmap[num]=1
+        
+        items = hashmap.items()
+        items.sort(key = lambda x: x[1], reverse = True)
+        result = []
+        for i in range(k):
+            result.append(items[i][0])
+
+        return result
