d55e35064e4a9492f88ef42129e7163f4b9d74e0
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-26 15:06:01 +0530

    Create 0931_minimum_falling_path_sum.py

---
+++ Python/0931_minimum_falling_path_sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minFallingPathSum(self, matrix):
+        n = len(matrix)
+        for i in range(1,n):
+            for j in range(n):
+                up = matrix[i-1][j]
+                left = matrix[i-1][j-1] if j>0 else float('inf')
+                right = matrix[i-1][j+1] if j<n-1 else float('inf')
+
+                matrix[i][j] += min(up,left,right)
+
+        return min(matrix[-1])
+        
