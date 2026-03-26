8d715279eefc3b4ef950e9271370d4daee1f1920
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-26 14:11:57 +0530

    Create 1005_maximum_sum_of_array_after_k_negations.py

---
+++ Python/1005_maximum_sum_of_array_after_k_negations.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestSumAfterKNegations(self, nums, k):
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i]<0 and k>0:
+                nums[i]*=-1
+                k-=1
+        
+        nums.sort()
+        if k%2==1:
+            nums[0]*=-1
+        return sum(nums)
