77034c56c97c796fc6cb78f33959b9eb0a741de6
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-04-15 20:20:16 +0530

    Refactor findMaxAverage for improved readability

--- Python/0643_maximum_average_subarray_I.py
+++ Python/0643_maximum_average_subarray_I.py
@@ -1,15 +1,10 @@
 class Solution(object):
     def findMaxAverage(self, nums, k):
-        window_sum=0
-        for i in range(k):
-            window_sum+=nums[i]
-        max_sum = window_sum
-        for i in range(k, len(nums)):
-            window_sum+=nums[i]
-            window_sum-=nums[i-k]
-            max_sum = window_sum if window_sum > max_sum else max_sum
-        
-        return max_sum/float(k)
-                
-
+        sum1 = sum(nums[0:k])
+        maxsum = sum1
+        for i in range(k,len(nums)):
+            sum1-=nums[i-k]
+            sum1+=nums[i]
+            maxsum = sum1 if sum1 > maxsum else maxsum
+        return maxsum/float(k)
         
