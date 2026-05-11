aeb3a24158e075c8dfeafa2903102b79a6f35b33
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-05-11 10:42:38 +0530

    Update 0977_squares_of_a_sorted_array.py

--- Python/0977_squares_of_a_sorted_array.py
+++ Python/0977_squares_of_a_sorted_array.py
@@ -4,3 +4,25 @@ class Solution(object):
             nums[i] = nums[i]**2
         nums.sort()
         return nums
+
+#or
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        n = len(nums)
+        ans = [0]*n
+        left = 0
+        right = n-1
+        pos = n-1
+        while left<=right:
+            ls = nums[left]**2
+            rs = nums[right]**2
+            if ls > rs:
+                ans[pos] = ls
+                left+=1
+            else:
+                ans[pos] = rs
+                right -=1
+
+            pos -=1
+        return ans
