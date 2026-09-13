0bb1bd7778657bcea62a1cba6beac18438bbd732
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-09-13 12:58:40 +0530

    Create Python0832_flipping_an_image.py

---
+++ Python0832_flipping_an_image.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in range(len(image)):
+            image[i].reverse()
+            for j in range(len(image[i])):
+                if image[i][j]==0:
+                    image[i][j]=1
+                else:
+                    image[i][j]=0
+        return image
+        
