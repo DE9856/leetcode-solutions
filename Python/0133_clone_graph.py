44a1d52e44410133af2768588203adfabfebc1d6
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-09-19 19:53:38 +0530

    Create 0133_clone_graph.py

---
+++ Python/0133_clone_graph.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+        clones = {}
+        def dfs(node):
+            if node in clones:
+                return clones[node]
+            copy = Node(node.val)
+            clones[node] = copy
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
+        return dfs(node)
+        
